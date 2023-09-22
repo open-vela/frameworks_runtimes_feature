668d8dd30cc54311819d59a0908086005eb132b6
openvela-robot <openvela-robot@xiaomi.com>
2023-09-22 16:04:42 +0800

    feature_framework: fix the memory leaky and issue of FeatureManagerWamr
    Signed-off-by: wujinfeng <wujinfeng@xiaomi.com>

--- tests/jidl/ajs_features_list.h
+++ tests/jidl/ajs_features_list.h
@@ -4,4 +4,5 @@ jse_Simple_initFeature(this, features);
 jse_Record_initFeature(this, features);
 jse_Struct_initFeature(this, features);
 jse_Promise_initFeature(this, features);
+jse_ATest_initFeature(this, features);
 jse_Interface_initFeature(this, features);
