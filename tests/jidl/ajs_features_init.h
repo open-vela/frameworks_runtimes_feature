972c780bc8426b6620a25905c666f15105450fd6
gaoyulong <gaoyulong@xiaomi.com>
2023-09-26 11:56:27 +0800

    restore interface and comment mockatest
    Signed-off-by: gaoyulong <gaoyulong@xiaomi.com>

--- tests/jidl/ajs_features_init.h
+++ tests/jidl/ajs_features_init.h
@@ -5,10 +5,11 @@ using namespace ferry;
 #define QAPPFEATURE_INIT(module) bool jse_##module##_initFeature(ferry::FeatureRegistry *mgr, std::vector<std::string>&features)
 
 bool jse_timers_initFeature(ferry::FeatureRegistry *mgr, std::vector<std::string>&features);
+bool jse_interface_initFeature(ferry::FeatureRegistry *mgr, std::vector<std::string>&features);
 bool jse_Simple_initFeature(ferry::FeatureRegistry *mgr, std::vector<std::string>&features);
 bool jse_Record_initFeature(ferry::FeatureRegistry *mgr, std::vector<std::string>&features);
 bool jse_Struct_initFeature(ferry::FeatureRegistry *mgr, std::vector<std::string>&features);
 bool jse_Promise_initFeature(ferry::FeatureRegistry *mgr, std::vector<std::string>&features);
 bool jse_ATest_initFeature(ferry::FeatureRegistry *mgr, std::vector<std::string>&features);
 bool jse_Interface_initFeature(ferry::FeatureRegistry *mgr, std::vector<std::string>&features);
-bool jse_mockatest_initFeature(ferry::FeatureRegistry *mgr, std::vector<std::string>&features);
+// bool jse_mockatest_initFeature(ferry::FeatureRegistry *mgr, std::vector<std::string>&features);
