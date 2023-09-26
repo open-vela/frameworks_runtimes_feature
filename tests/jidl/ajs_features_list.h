7022cfeb848128e60c05024f0a612b174ad95d07
gaoyulong <gaoyulong@xiaomi.com>
2023-09-26 11:56:27 +0800

    restore interface and comment mockatest
    Signed-off-by: gaoyulong <gaoyulong@xiaomi.com>

--- tests/jidl/ajs_features_list.h
+++ tests/jidl/ajs_features_list.h
@@ -1,8 +1,9 @@
 jse_timers_initFeature(this, features);
+jse_interface_initFeature(this, features);
 jse_Simple_initFeature(this, features);
 jse_Record_initFeature(this, features);
 jse_Struct_initFeature(this, features);
 jse_Promise_initFeature(this, features);
 jse_ATest_initFeature(this, features);
 jse_Interface_initFeature(this, features);
-jse_mockatest_initFeature(this, features);
+//jse_mockatest_initFeature(this, features);
