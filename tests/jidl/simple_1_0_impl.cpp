d73db5fa15fe583545910a7badbcbc967b63dcd3
daichuan <daichuan@xiaomi.com>
2023-10-11 21:54:33 +0800

    modify for codereview and warning with vela sim compile
    Signed-off-by: daichuan <daichuan@xiaomi.com>

--- tests/jidl/simple_1_0_impl.cpp
+++ tests/jidl/simple_1_0_impl.cpp
@@ -112,8 +112,8 @@ void Simple_wrap_print(FeatureInstanceHandle feature, AppendData data, FtVariPar
         } else if (param_type == FT_TYPE_ARRAY) {
             uint32_t array_size = ft_array_size(ft_ctx, param);
             printf("[");
-            for (int i = 0; i < array_size; ++i) {
-                ft_value_t elem = ft_array_at(ft_ctx, param, i);
+            for (uint32_t j = 0; j < array_size; ++j) {
+                ft_value_t elem = ft_array_at(ft_ctx, param, j);
                 ft_type elem_type = ft_get_type(ft_ctx, elem);
                 if (elem_type == FT_TYPE_NUMBER) {
                     double param_num;
@@ -125,7 +125,7 @@ void Simple_wrap_print(FeatureInstanceHandle feature, AppendData data, FtVariPar
                     ft_free_string(ft_ctx, param_str);
                 } else if (elem_type == FT_TYPE_BOOL) {
                     bool param_bool;
-                    bool ret = ft_to_bool(ft_ctx, param, &param_bool);
+                    ft_to_bool(ft_ctx, param, &param_bool);
                     printf("%d ", param_bool);
                 } else {
                     printf("invalid array element type!");
@@ -139,11 +139,11 @@ void Simple_wrap_print(FeatureInstanceHandle feature, AppendData data, FtVariPar
             ft_free_string(ft_ctx, param_str);
         } else if (param_type == FT_TYPE_NUMBER) {
             double param_num;
-            bool ret = ft_to_double(ft_ctx, param, &param_num);
+            ft_to_double(ft_ctx, param, &param_num);
             printf("%lf ", param_num);
         } else if (param_type == FT_TYPE_BOOL) {
             bool param_bool;
-            bool ret = ft_to_bool(ft_ctx, param, &param_bool);
+            ft_to_bool(ft_ctx, param, &param_bool);
             printf("%d ", param_bool);
         } else {
             printf("invalid param type!");
@@ -178,8 +178,8 @@ void Simple_wrap_bar5(FeatureInstanceHandle feature, AppendData data, FtInt a, F
         } else if (param_type == FT_TYPE_ARRAY) {
             uint32_t array_size = ft_array_size(ft_ctx, param);
             printf("[");
-            for (int i = 0; i < array_size; ++i) {
-                ft_value_t elem = ft_array_at(ft_ctx, param, i);
+            for (uint32_t j = 0; j < array_size; ++j) {
+                ft_value_t elem = ft_array_at(ft_ctx, param, j);
                 ft_type elem_type = ft_get_type(ft_ctx, elem);
                 if (elem_type == FT_TYPE_NUMBER) {
                     double param_num;
@@ -191,7 +191,7 @@ void Simple_wrap_bar5(FeatureInstanceHandle feature, AppendData data, FtInt a, F
                     ft_free_string(ft_ctx, param_str);
                 } else if (elem_type == FT_TYPE_BOOL) {
                     bool param_bool;
-                    bool ret = ft_to_bool(ft_ctx, param, &param_bool);
+                    ft_to_bool(ft_ctx, param, &param_bool);
                     printf("%d ", param_bool);
                 } else {
                     printf("invalid array element type!");
@@ -205,11 +205,11 @@ void Simple_wrap_bar5(FeatureInstanceHandle feature, AppendData data, FtInt a, F
             ft_free_string(ft_ctx, param_str);
         } else if (param_type == FT_TYPE_NUMBER) {
             double param_num;
-            bool ret = ft_to_double(ft_ctx, param, &param_num);
+            ft_to_double(ft_ctx, param, &param_num);
             printf("%lf ", param_num);
         } else if (param_type == FT_TYPE_BOOL) {
             bool param_bool;
-            bool ret = ft_to_bool(ft_ctx, param, &param_bool);
+            ft_to_bool(ft_ctx, param, &param_bool);
             printf("%d ", param_bool);
         } else {
             printf("invalid param type!");
--- tests/jidl/struct_1_0_impl.cpp
+++ tests/jidl/struct_1_0_impl.cpp
@@ -140,8 +140,8 @@ void Struct_wrap_print(FeatureInstanceHandle feature, AppendData data, FtVariPar
         } else if (param_type == FT_TYPE_ARRAY) {
             uint32_t array_size = ft_array_size(ft_ctx, param);
             printf("[");
-            for (int i = 0; i < array_size; ++i) {
-                ft_value_t elem = ft_array_at(ft_ctx, param, i);
+            for (int j = 0; j < array_size; ++j) {
+                ft_value_t elem = ft_array_at(ft_ctx, param, j);
                 ft_type elem_type = ft_get_type(ft_ctx, elem);
                 if (elem_type == FT_TYPE_NUMBER) {
                     double param_num;
@@ -153,7 +153,7 @@ void Struct_wrap_print(FeatureInstanceHandle feature, AppendData data, FtVariPar
                     ft_free_string(ft_ctx, param_str);
                 } else if (elem_type == FT_TYPE_BOOL) {
                     bool param_bool;
-                    bool ret = ft_to_bool(ft_ctx, param, &param_bool);
+                    ft_to_bool(ft_ctx, param, &param_bool);
                     printf("%d ", param_bool);
                 } else {
                     printf("invalid array element type!");
@@ -167,11 +167,11 @@ void Struct_wrap_print(FeatureInstanceHandle feature, AppendData data, FtVariPar
             ft_free_string(ft_ctx, param_str);
         } else if (param_type == FT_TYPE_NUMBER) {
             double param_num;
-            bool ret = ft_to_double(ft_ctx, param, &param_num);
+            ft_to_double(ft_ctx, param, &param_num);
             printf("%lf ", param_num);
         } else if (param_type == FT_TYPE_BOOL) {
             bool param_bool;
-            bool ret = ft_to_bool(ft_ctx, param, &param_bool);
+            ft_to_bool(ft_ctx, param, &param_bool);
             printf("%d ", param_bool);
         } else {
             printf("invalid param type!");
