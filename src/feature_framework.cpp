f1f5a408abbd01b3f4d1cec16f664b97f10f6ab5
openvela-robot <openvela-robot@xiaomi.com>
2023-10-19 19:25:52 +0800

    JIDL: fix the issue that the json node of the interface extends feild lost
    Signed-off-by: wujinfeng <wujinfeng@xiaomi.com>

--- src/feature_framework.cpp
+++ src/feature_framework.cpp
@@ -198,14 +198,4 @@ void FeaturePrototype::clearAllInstances()
     instances.clear();
 }
 
-void FeaturePrototype::setPackageName(char* package_name)
-{
-    package_name_ = package_name;
-}
-
-char* FeaturePrototype::getPackageName() const
-{
-    return package_name_;
-}
-
 }
--- src/feature_framework.h
+++ src/feature_framework.h
@@ -103,13 +103,6 @@ public:
      *
      */
     void clearAllInstances();
-
-    void setPackageName(char* package_name);
-
-    char* getPackageName() const;
-
-private:
-    char* package_name_ = nullptr;
 };
 
 /**
