914fb368db0b1d1499fc8f64290afea14d4771e2
openvela-robot <openvela-robot@xiaomi.com>
2023-10-27 12:59:09 +0800

    optimize the code style of FeatureManagerQjs class
    Signed-off-by: wujinfeng <wujinfeng@xiaomi.com>

--- tests/jidl/ajs_features_list.h
+++ tests/jidl/ajs_features_list.h
@@ -4,6 +4,5 @@ jse_Simple_initFeature(this, features);
 jse_Record_initFeature(this, features);
 jse_struct_test_initFeature(this, features);
 jse_promise_test_initFeature(this, features);
-jse_ATest_initFeature(this, features);
 jse_interface_test_initFeature(this, features);
 //jse_mockatest_initFeature(this, features);
