ea93d35fb554ac79250299f5fbf3240b9f7f1093
gaoyulong <gaoyulong@xiaomi.com>
2023-10-31 15:34:03 +0800

    Test Framework: change require to cfunctiondata
    Signed-off-by: gaoyulong <gaoyulong@xiaomi.com>

--- tests/jidl/test_main.cpp
+++ tests/jidl/test_main.cpp
@@ -3,17 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "feat_test_utils.h"
 #include "feature_log.h"
 #include "feature_manager_qjs.h"
 #include "feature_registry.h"
-#include "feat_test_utils.h"
-
 
 using namespace ferry;
 using namespace FEATURE;
-
-// need remove global variable later
-ferry::FeatureManagerQjs* g_manager = nullptr;
 
 typedef struct feature_env_t {
   JSRuntime* rt;
@@ -22,15 +18,19 @@ typedef struct feature_env_t {
 
 // __require
 feature_value_t __require(feature_context_ref ctx, feature_value_t this_val,
-                          int argc, feature_value_t* argv) {
+                          int argc, feature_value_t* argv, int magic,
+                          feature_value_t* func_data) {
   if (argc < 1) {
     FEATURE_THROW_INTERNAL_ERROR(ctx, "require need module name!");
     return FEATURE_UNDEFINED;
   }
 
+  ferry::FeatureManagerQjs* manager = static_cast<ferry::FeatureManagerQjs*>(
+      feature_get_opaque(func_data[0], 1));
+
   const char* str_module_name = feature_to_cstring(ctx, argv[0]);
   feature_value_t vm_object = JS_UNDEFINED;
-  auto feature_obj = g_manager->featureRequire(ctx, vm_object, str_module_name);
+  auto feature_obj = manager->featureRequire(ctx, vm_object, str_module_name);
   feature_free_cstring(ctx, str_module_name);
   return feature_obj;
 }
@@ -87,8 +87,7 @@ bool load_file(const char* file_name, char** file_content) {
 //   }
 // }
 
-typedef void (*uv_prepare_callback)(uv_prepare_t*);
-// typedef void (*uv_check_callback)(uv_check_t*);
+// TODO: remove stop
 static void execute_job_cb(uv_prepare_t* handle) {
   feature_env_t* env = static_cast<feature_env_t*>(handle->data);
   feature_context_ref r_ctx;
@@ -110,13 +109,22 @@ static void run_loop(ferry::FeatureManager* manager) {
 }
 
 static void stop_loop(ferry::FeatureManager* manager) {
-    uv_loop_t* ploop = manager->getUVLoop();
-    uv_stop(ploop);
+  uv_loop_t* ploop = manager->getUVLoop();
+  uv_stop(ploop);
+}
+
+static feature_value_t getRequireObject(feature_env_t* js_env,
+                                        ferry::FeatureManagerQjs* m) {
+  feature_value_t func_data = feature_object(js_env->ctx);
+  feature_set_opaque(func_data, m);
+  feature_value_t ret =
+      feature_cfunctiondata(js_env->ctx, __require, 1, 0, 1, &func_data);
+  feature_free_value(js_env->ctx, func_data);
+  return ret;
 }
 
 // 支持cli来读取 js 文件去执行，命令为：./feature_jidl_test
 int main(int argc, char** argv) {
-//   ferry::FeatureManagerQjs* manager;
   if (argc < 2) {
     printf("please input js file, like ./test.js \n");
     return 0;
@@ -127,9 +135,10 @@ int main(int argc, char** argv) {
   char* test_str = NULL;
   char* manifast_str = NULL;
 
-  // this path should change depends
+  // TODO: ues qjs bytecode
   const char* test_file =
-      "/home/gyl/work/vela/miwear-bes/frameworks/base/feature/tests/jidl/test-internal.js";
+      "/home/gyl/work/vela/miwear-bes/frameworks/base/feature/tests/jidl/"
+      "test-internal.js";
   // 打开 test 框架加载文件
   load_file(test_file, &test_str);
   if (test_str == NULL) {
@@ -149,8 +158,6 @@ int main(int argc, char** argv) {
 
   // initialize quickjs engine
   feature_env_t js_env;
-//   uv_loop_t loop;
-//   uv_loop_init(&loop);
   uv_prepare_t prepare;
 
   uv_prepare_init(uv_default_loop(), &prepare);
@@ -163,19 +170,20 @@ int main(int argc, char** argv) {
   JS_SetRuntimeOpaque(js_env.rt, js_env.ctx);
   auto registry = new ferry::FeatureRegistry();
   registry->init(nullptr);
-  // g_manager = CreateFeatureManager(registry, "js");
-  g_manager = new ferry::FeatureManagerQjs(registry);
 
-  struct LoopPack pack = {g_manager, run_loop, stop_loop};
+  // TODO: use factory pattern: manager = CreateFeatureManager(registry, "js");
+  ferry::FeatureManagerQjs* manager = new ferry::FeatureManagerQjs(registry);
 
-  g_manager->setUserData("run_loop", &pack);
+  struct LoopPack pack = {manager, run_loop, stop_loop};
 
-  g_manager->setUVLoop(uv_default_loop());
+  manager->setUserData("run_loop", &pack);
+
+  manager->setUVLoop(uv_default_loop());
 
   // register global require
   feature_value_t global_obj = feature_global_object(js_env.ctx);
-  feature_value_t require =
-      feature_cfunction(js_env.ctx, __require, "require", 0);
+  feature_value_t require = getRequireObject(&js_env, manager);
+
   feature_set_object_property(js_env.ctx, global_obj, "require", require);
   feature_free_value(js_env.ctx, global_obj);
 
@@ -184,7 +192,7 @@ int main(int argc, char** argv) {
                           JS_EVAL_TYPE_GLOBAL);
   if (JS_IsException(res)) {
     const char* str = JS_ToCString(js_env.ctx, res);
-        printf("test internal file error: %s\n", str);
+    printf("test internal file error: %s\n", str);
     feature_free_value(js_env.ctx, res);
     return -1;
   }
@@ -194,22 +202,17 @@ int main(int argc, char** argv) {
                              JS_EVAL_TYPE_MODULE | JS_EVAL_FLAG_STRICT);
 
   if (JS_IsException(result)) {
-    const char* str = JS_ToCString(js_env.ctx, result); 
-        printf("exec js file error: %s\n", str);
+    const char* str = JS_ToCString(js_env.ctx, result);
+    printf("exec js file error: %s\n", str);
     feature_free_value(js_env.ctx, result);
     return -1;
   }
 
   uv_run(uv_default_loop(), UV_RUN_DEFAULT);
 
-//   uv_prepare_stop(&prepare);
-//   uv_check_stop(&checker);
-//   uv_close((uv_handle_t*)&prepare, NULL);
-//   uv_close((uv_handle_t*)&checker, NULL);
-
   feature_free_value(js_env.ctx, result);
   // release manager first
-  dynamic_cast<ferry::FeatureManagerQjs*>(g_manager)->uninit();
+  manager->uninit();
   JS_FreeContext(js_env.ctx);
   JS_FreeRuntime(js_env.rt);
   uv_loop_close(uv_default_loop());
@@ -223,8 +226,8 @@ int main(int argc, char** argv) {
     free(js_str);
     js_str = NULL;
   }
-  // free g_manager
-  delete g_manager;
+  // free manager
+  delete manager;
 
   return 0;
 }
