940114bca66fbaf76ce4d5a0b57afc8faacc3796
wangyingdong <wangyingdong@xiaomi.com>
2023-11-21 10:19:08 +0800

    add fetch feature
    Signed-off-by: wangyingdong <wangyingdong@xiaomi.com>

---
+++ modules/net_utils.h
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2023 Xiaomi Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef _NET_UTILS_H_
+#define _NET_UTILS_H_
+#include <stdbool.h>
+
+#include <map>
+
+#include "feature.h"
+#include "feature_types.h"
+#include "netutils/cJSON.h"
+#include "uv_ext.h"
+
+// namespace NET {
+typedef enum ErrorCode {
+  GENERAL = 200,
+  ARGSERROR = 202,
+  TIMEOUT = 204,
+  IOERROR = 300
+} ErrorCode;
+
+#define SET_ARGERROR(x, _msg) \
+  do {                        \
+    if (!(x)) {               \
+      msg = _msg;             \
+      code = ARGSERROR;       \
+      goto err;               \
+    }                         \
+  } while (0)
+
+#define SET_JS_ERROR(x, _code, _msg) \
+  do {                               \
+    if (!(x)) {                      \
+      msg = _msg;                    \
+      code = _code;                  \
+      goto err;                      \
+    }                                \
+  } while (0)
+
+#define ASSERT_RET_ECHO(x, fmt, ...)             \
+  do {                                           \
+    if (!(x)) {                                  \
+      FEATURE_LOG_ERROR(TAG fmt, ##__VA_ARGS__); \
+      return;                                    \
+    }                                            \
+  } while (0)
+
+#define ASSERT_RET(x) \
+  do {                \
+    if (!(x)) {       \
+      return;         \
+    }                 \
+  } while (0)
+
+#define ASSERT_RET_NULL(x) \
+  do {                     \
+    if (!(x)) {            \
+      return NULL;         \
+    }                      \
+  } while (0)
+
+#define REQUEST_LIST_FOR_EVERY(head, _type) \
+  _type *req, *_temp;                       \
+  weakref_list_for_every_entry_safe(head, req, _temp, _type, node)
+
+#define GET_FEATURE_AND_CTX(x)                  \
+  FeatureInstanceHandle feature = (x)->feature; \
+  ft_context_ref ft_ctx = (x)->ft_ctx;
+
+#define INIT_CB (-1)
+#define REQUEST_CANCEL 2
+#define USER_ABORT_MSG "user cancel request"
+#define USER_ABORT_MSG_SIZE (strlen(USER_ABORT_MSG) + 1)
+#define CANCEL_ERROR_CODE 1002
+
+typedef struct {
+  uv_request_session_t* handle;
+  const char* pkg;
+  struct weakref_list_node linklist;
+} request_context_t;
+
+bool check_url(FtString url);
+request_context_t* get_request_context(FeatureInstanceHandle feature);
+
+bool ft_map_for_every_entry(ft_context_ref ft_ctx, FtAny data, void* userp,
+                            bool (*user_cb)(const cJSON* const item,
+                                            void* userp));
+bool check_header(ft_context_ref ft_ctx, FtAny js_headers,
+                  std::map<std::string, std::string>& headers);
+
+// }
+
+#endif
---
+++ tests/jidl/fetch.js
@@ -0,0 +1,82 @@
+let fetch = require('fetch');
+
+feat_async_test("fetch", "fetch", (done) => {
+  return new Promise(function (resolve, reject) {
+    fetch.fetch({
+      'url': 'http://httpbin.org/get',
+      'data': {},
+      'header':{},
+      'method': "",
+      'responseType': '',
+      'timeout': 10000,
+      'success': function (response) {
+        print("hik succ headers");
+        print(response.statusCode);
+        //
+        print(JSON.stringify(response.data));
+        print("hik succ data");
+        print(JSON.stringify(response.headers));
+        resolve(true);
+      },
+      'fail': function (data, code) {
+        print("hik err");
+        resolve(true);
+      },
+      'complete': function () {
+        print('hik complete');
+      }
+    });
+  }).then((res) => {
+    print('hik then');
+    feat_expect_true(res, "fetch  success");
+    done();
+  }, (err) => {
+    feat_expect_true(err, "fetch  fail");
+    done();
+  });
+
+})
+
+
+feat_async_test("fetch", "fetch", (done) => {
+  return new Promise(function (resolve, reject) {
+    fetch.fetch({
+      'url': 'https://wis.qq.com/weather/common?source=pc&province=%E5%8C%97%E4%BA%AC&city=%E5%8C%97%E4%BA%AC&country=%E5%8C%97%E4%BA%AC&weather_type=forecast_1h',
+      'responseType': 'json',
+      // 'data': b,
+      'header':{"Content-Type":"application/x-www-form-urlencoded"},
+      'method': "",
+      'responseType': '',
+      'timeout': 10000,
+      'success': function (response) {
+        // const obj = response.data;
+        // self.result2 = JSON.stringify(obj.data.forecast_1h[0]);
+        print('the status code of the response: ${response.code}');
+        print(response.code);
+        print('the data of the response: ${response.data}');
+        print(response.data)
+        print('the headers of the response: ${JSON.stringify(response.headers)}');
+        print(JSON.stringify(response.headers));
+        resolve(true);
+      },
+      'fail': function (data, code) {
+        print('handling fail, errMsg = ${data}');
+        print(data);
+        print('handling fail, errCode = ${data}');
+        print(true);
+        resolve(false);
+      },
+      'complete': function () {
+        print('hik complete');
+      }
+    });
+  }).then((res) => {
+    print('hik then');
+    feat_expect_true(res, "fetch  success");
+    done();
+  }, (err) => {
+    feat_expect_true(err, "fetch  fail");
+    done();
+  });
+
+})
