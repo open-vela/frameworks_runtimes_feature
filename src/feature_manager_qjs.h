c3ab7bcae1620b9e336f726d3d57a9d007c9dcb1
wujinfeng <wujinfeng@xiaomi.com>
2023-11-27 19:02:51 +0800

    change the define of the createFeature function in FeatureManagerQjs
    Signed-off-by: wujinfeng <wujinfeng@xiaomi.com>

--- src/feature_manager_qjs.h
+++ src/feature_manager_qjs.h
@@ -50,7 +50,7 @@ public:
 
     feature_value_t findFeature(feature_context_ref ctx, const char* name);
 
-    feature_value_t createFeature(feature_context_ref ctx, feature_value_t js_proto);
+    feature_value_t createFeature(feature_context_ref ctx, feature_value_t proto, feature_value_t vm_object);
 
     feature_value_t createJsInstance(FeaturePrototype* prototype, FeatureInstanceQjs* interface);
 
