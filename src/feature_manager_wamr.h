aa58fd379309f9731dd07fbdb5b33b3afd7b2921
wujinfeng <wujinfeng@xiaomi.com>
2024-01-15 17:25:36 +0800

    fix feature name issue
    Signed-off-by: wujinfeng <wujinfeng@xiaomi.com>

--- src/feature_manager_wamr.h
+++ src/feature_manager_wamr.h
@@ -51,7 +51,6 @@ private:
     void* wamr_env_;
     std::vector<NativeSymbol*> native_symbols_;
     std::vector<char*> native_strings_;
-    std::map<std::string, std::string> class_name_map_;
 };
 
 }
