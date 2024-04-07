8da3cd819c410b349845c358c3132fffebac8d21
zhuzhihai <zhuzhihai@xiaomi.com>
2024-04-07 14:53:07 +0800

    Revert "Revert "feat: add update event interface for brightness.""
    This reverts commit 8927fb540991c7d726a2cca2959e86124d5195c1.
    
    Reason for revert: using the new brightness interface
    
    Signed-off-by: zhuzhihai <zhuzhihai@xiaomi.com>

--- modules/brightness_impl.cpp
+++ modules/brightness_impl.cpp
@@ -1,20 +1,68 @@
-extern "C" {
-#include "brightness_service.h"
-}
-#include "src/brightness.h"
-#include <cstddef>
 
-void system_brightness_onRegister(const char* feature_name)
-{
-}
+#include <binder/IServiceManager.h>
+#include <binder/ProcessState.h>
+
+#include <utils/Log.h>
+#include <utils/String8.h>
+
+#include "src/brightness.h"
+#include <BrightnessService.h>
+#include <cstddef>
+#include <os/brightness/BnBrightnessObserver.h>
+#include <os/brightness/IBrightnessService.h>
+
+class MonitorBrightnessCallback : public os::brightness::BnBrightnessObserver {
+public:
+    android::binder::Status onBrightnessChanged(int32_t level) override
+    {
+        if (feature == nullptr) {
+            return android::binder::Status::fromExceptionCode(android::binder::Status::Exception::EX_NULL_POINTER);
+        }
+        ft_context_ref ft_ctx = FeatureGetContext(feature);
+        ft_value_t ret_obj = ft_new_object(ft_ctx);
+        ft_value_t ret_level = ft_from_int(ft_ctx, level);
+        ft_obj_set_property(ft_ctx, ret_obj, "value", ret_level);
+
+        FeatureInvokeCallback(feature, cid, &ret_obj);
+        return android::binder::Status::ok();
+    }
+    FeatureInstanceHandle feature {};
+    FtCallbackId cid {};
+};
+
+android::sp<os::brightness::IBrightnessService> service;
+android::sp<MonitorBrightnessCallback> callback;
+
+void system_brightness_onRegister(const char* feature_name) { }
 void system_brightness_onCreate(FeatureRuntimeContext ctx,
     FeatureProtoHandle handle) { }
 void system_brightness_onRequired(FeatureRuntimeContext ctx,
-    FeatureInstanceHandle handle) { }
+    FeatureInstanceHandle handle)
+{
+    android::sp<android::IServiceManager> sm(android::defaultServiceManager());
+    FEATURE_LOG_INFO("defaultServiceManager(): %p", sm.get());
+
+    // obtain brightness.service
+    android::sp<android::IBinder> binder = sm->getService(os::brightness::BrightnessService::name());
+    if (binder == NULL) {
+        FEATURE_LOG_INFO("brightness service binder is null, abort...");
+        return;
+    }
+    FEATURE_LOG_INFO("brightness service binder is %p", binder.get());
+    service = android::interface_cast<os::brightness::IBrightnessService>(binder);
+    FEATURE_LOG_INFO("brightness service is %p", service.get());
+}
 void system_brightness_onDetached(FeatureRuntimeContext ctx,
     FeatureInstanceHandle handle) { }
 void system_brightness_onDestroy(FeatureRuntimeContext ctx,
-    FeatureProtoHandle handle) { }
+    FeatureProtoHandle handle)
+{
+    if (service) {
+        service->unmonitorBrightness(callback);
+        callback = nullptr;
+        service = nullptr;
+    }
+}
 void system_brightness_onUnregister(const char* feature_name) { }
 
 static void do_callback(FeatureInstanceHandle feature, int value, int code,
@@ -40,17 +88,30 @@ void system_brightness_wrap_getValue(FeatureInstanceHandle feature,
     union AppendData append_data,
     system_brightness_GetValueParam* param)
 {
-    int ret = brightness_get_current_level();
-    do_callback(feature, ret, ret, param->success, param->fail, param->complete);
+    if (!service) {
+        FEATURE_LOG_ERROR("FATAL: service == NULL");
+        return;
+    }
+    int32_t level;
+    auto status = service->getTargetBrightness(&level);
+    FEATURE_LOG_INFO("brightness target level is %d", level);
+
+    do_callback(feature, level, status.isOk() ? 0 : -1, param->success, param->fail, param->complete);
 }
 
 void system_brightness_wrap_setValue(FeatureInstanceHandle feature,
     union AppendData append_data,
     system_brightness_SetValueParam* param)
 {
+    if (!service) {
+        FEATURE_LOG_ERROR("FATAL: service == NULL");
+        return;
+    }
     int ret = -1;
     if (param->value >= 0 && param->value <= 255) {
-        ret = brightness_set_target(brightness_get_system_session(), param->value, 0);
+        auto status = service->setTargetBrightness(param->value, 0);
+        ret = status.isOk() ? 0 : -1;
+        FEATURE_LOG_INFO("brightness target level is %d", param->value);
     }
     do_callback(feature, ret, ret, param->success, param->fail, param->complete);
 }
@@ -59,8 +120,14 @@ void system_brightness_wrap_getMode(FeatureInstanceHandle feature,
     union AppendData append_data,
     system_brightness_GetModeParam* param)
 {
-    int ret = brightness_get_mode(brightness_get_system_session());
-    do_callback(feature, ret == BRIGHTNESS_MODE_MANUAL ? 0 : 1, ret,
+    if (!service) {
+        FEATURE_LOG_ERROR("FATAL: service == NULL");
+        return;
+    }
+    os::brightness::Mode mode;
+    auto status = service->getBrightnessMode(&mode);
+    FEATURE_LOG_INFO("brightness target mode is %d", (int)mode);
+    do_callback(feature, !(int)mode, status.isOk() ? 0 : -1,
         param->success, param->fail, param->complete);
 }
 
@@ -68,11 +135,17 @@ void system_brightness_wrap_setMode(FeatureInstanceHandle feature,
     union AppendData append_data,
     system_brightness_SetModeParam* param)
 {
+    if (!service) {
+        FEATURE_LOG_ERROR("FATAL: service == NULL");
+        return;
+    }
     int ret = -1;
     if (param->mode == 0 || param->mode == 1) {
-        ret = brightness_set_mode(brightness_get_system_session(), param->mode == 0 ? BRIGHTNESS_MODE_MANUAL : BRIGHTNESS_MODE_AUTO);
+        auto status = service->setBrightnessMode(param->mode == 1 ? os::brightness::Mode::AUTO : os::brightness::Mode::MANUAL);
+        ret = status.isOk() ? 0 : -1;
     }
     do_callback(feature, ret, ret, param->success, param->fail, param->complete);
+    FEATURE_LOG_INFO("brightness target mode is %d", param->mode);
 }
 
 void system_brightness_wrap_setKeepScreenOn(
@@ -80,4 +153,16 @@ void system_brightness_wrap_setKeepScreenOn(
     system_brightness_SetKeepScreenOnParam* param)
 {
     // TODO
+}
+
+void system_brightness_set_onbrightnesschanged(FeatureInstanceHandle feature, union AppendData append_data, FtCallbackId cb)
+{
+    if (!service) {
+        FEATURE_LOG_ERROR("FATAL: service == NULL");
+        return;
+    }
+    callback = android::sp<MonitorBrightnessCallback>::make();
+    callback->feature = feature;
+    callback->cid = cb;
+    service->monitorBrightness(callback);
 }
