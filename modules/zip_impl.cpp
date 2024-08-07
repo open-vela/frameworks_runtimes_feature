f39952e771f79f356b0c7e43fb4871662e821010
zhangliangyu3 <zhangliangyu3@xiaomi.com>
2024-08-07 13:44:16 +0800

    feature : fix memory leak
    Signed-off-by: zhangliangyu3 <zhangliangyu3@xiaomi.com>

--- modules/zip_impl.cpp
+++ modules/zip_impl.cpp
@@ -143,7 +143,7 @@ static void detach(FeatureInstanceHandle handle)
             /* task not start, zc->zr_arr[i] not null, should be cancel the task when page destory */
             if (zc->zr_arr[i] != nullptr) {
                 uv_cancel((uv_req_t*)(&(zc->zr_arr[i]->req)));
-                zc->zr_arr[i] = nullptr;
+                freeZipReq(zc->zr_arr[i]);
             }
         }
         delete zc;
@@ -333,7 +333,7 @@ static void clearZrPtr(std::vector<zipReq*>& zr_arr, zipReq* zr)
 {
     for (size_t i = 0; i < zr_arr.size(); ++i) {
         if (zr_arr[i] == zr) {
-            zr_arr[i] = nullptr;
+            freeZipReq(zr_arr[i]);
         }
     }
     return;
