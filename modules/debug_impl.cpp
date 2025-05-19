a6120047fc6a55f8f8fedaa6ecd9494bc6b768cb
v-fengxuefeng <v-fengxuefeng@xiaomi.com>
2025-05-19 20:22:05 +0800

    add debug feature for miwear debug
    add debug feature for miwear debug
    
    Signed-off-by: v-fengxuefeng <v-fengxuefeng@xiaomi.com>

---
+++ modules/debug_impl.cpp
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2023 Xiaomi Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	 http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <kvdb.h>
+
+#include "feature_exports.h"
+#include "feature_log.h"
+#include "feature_types.h"
+
+#define SYSTEMDEBUG_LIFECYCLE_DEBUG() \
+    FEATURE_LOG_DEBUG("[jidl_feature] debug_impl:: %s()", __FUNCTION__)
+
+#define APPDEBUGKEY "persist.quickapp.debug"
+
+void system_debug_onRegister(const char* feature_name)
+{
+    SYSTEMDEBUG_LIFECYCLE_DEBUG();
+}
+void system_debug_onCreate(FeatureRuntimeContext ctx, FeatureProtoHandle handle)
+{
+    SYSTEMDEBUG_LIFECYCLE_DEBUG();
+    FEATURE_LOG_INFO("[jump native] system_debug_onCreate");
+}
+void system_debug_onRequired(FeatureRuntimeContext ctx,
+    FeatureInstanceHandle handle)
+{
+    SYSTEMDEBUG_LIFECYCLE_DEBUG();
+}
+void system_debug_onDetached(FeatureRuntimeContext ctx,
+    FeatureInstanceHandle handle)
+{
+    SYSTEMDEBUG_LIFECYCLE_DEBUG();
+}
+void system_debug_onDestroy(FeatureRuntimeContext ctx, FeatureProtoHandle handle)
+{
+    SYSTEMDEBUG_LIFECYCLE_DEBUG();
+}
+void system_debug_onUnregister(const char* feature_name)
+{
+    SYSTEMDEBUG_LIFECYCLE_DEBUG();
+}
+
+void system_debug_wrap_enable(FeatureInstanceHandle feature, AppendData append_data, FtPromiseId pid)
+{
+    FEATURE_LOG_INFO("[jump native] system_debug_wrap_enable");
+    int ret = property_set_int32(APPDEBUGKEY, 1);
+    if (ret != 0) {
+        FEATURE_LOG_ERROR("[jump native] system_debug_wrap_enable property_set_bool error");
+        FeaturePromiseReject(feature, pid, ret, "property_set_key error");
+        return;
+    }
+    FeaturePromiseResolve(feature, pid, ret, "success");
+}
+
+void system_debug_wrap_disable(FeatureInstanceHandle feature, AppendData append_data, FtPromiseId pid)
+{
+    FEATURE_LOG_INFO("[jump native] system_debug_wrap_disable");
+    int ret = property_set_int32(APPDEBUGKEY, 0);
+    if (ret != 0) {
+        FEATURE_LOG_ERROR("[jump native] system_debug_wrap_disable property_set_bool error");
+        FeaturePromiseReject(feature, pid, ret, "property_set_key error");
+        return;
+    }
+    FeaturePromiseResolve(feature, pid, ret, "success");
+}
+
+void system_debug_wrap_getStatus(FeatureInstanceHandle feature, AppendData append_data, FtPromiseId pid)
+{
+    FEATURE_LOG_INFO("[jump native] system_debug_wrap_finish");
+    int ret = property_get_int32(APPDEBUGKEY, -1);
+    FeaturePromiseResolve(feature, pid, ret, "success");
+}
