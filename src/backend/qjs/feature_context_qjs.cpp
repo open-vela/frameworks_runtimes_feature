2c8884575f2b64b75eba5f35b31018014c640536
wujinfeng <wujinfeng@xiaomi.com>
2025-06-19 16:37:18 +0800

    add ft_ctx_to_js_ctx interface function
    Signed-off-by: wujinfeng <wujinfeng@xiaomi.com>

--- src/backend/qjs/feature_context_qjs.cpp
+++ src/backend/qjs/feature_context_qjs.cpp
@@ -138,3 +138,8 @@ JSValue ft_to_jsvalue(ft_context_ref rt_ctx, ft_value_t ft_val)
 {
     return FT_VAL_GET_JS_VAL(ft_val);
 }
+
+JSContext* ft_ctx_to_js_ctx(ft_context_ref rt_ctx)
+{
+    return (JSContext*)rt_ctx->data;
+}
