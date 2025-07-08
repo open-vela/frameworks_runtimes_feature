990ca80c5d7427f04d12f54e4188dc3c33588a87
zhangjun37 <zhangjun37@xiaomi.com>
2025-07-08 15:11:29 +0800

    feature_unit_test: add jidl testcase for interface
    Signed-off-by: zhangjun37 <zhangjun37@xiaomi.com>

---
+++ tests/unit/jidl/interface_test.js
@@ -0,0 +1,71 @@
+let test = require('unit_interface_test');
+init_feat_filter("interfaceTest.*");
+
+feat_test("interfaceTest", "dogTest", () => {
+    let dog = test.createDog(1);
+    feat_expect_true(dog !== undefined && dog !== null, "dogTest case 1 error!");
+    feat_expect_true(dog.name === "dog", "dogTest case 2 error!");
+    feat_expect_true(dog.legCount === 4, "dogTest case 3 error!");
+    dog.name = "Tom";
+    feat_expect_true(dog.name === "Tom", "dogTest case 4 error!");
+    // read only
+    dog.legCount = 0;
+    feat_expect_true(dog.legCount === 4, "dogTest case 5 error!");
+    feat_expect_true(dog.eatFood(["food", "food"]), "dogTest case 6 error!");
+    feat_expect_true(dog.run(100, "home"), "dogTest case 7 error!")
+    // para error
+    feat_expect_true(dog.eatFood() instanceof Error, "dogTest case 8 error!");
+    feat_expect_true(dog.run() instanceof Error, "dogTest case 9 error!");
+    feat_expect_true(dog.eatFood(100) instanceof Error, "dogTest case 9 error!");
+})
+
+// no ctor test
+feat_test("interfaceTest", "catTest", () => {
+    let cat = test.createCat();
+    feat_expect_true(cat !== undefined && cat !== null, "catTest case 1 error!");
+    feat_expect_true(cat.name === "cat", "catTest case 2 error!");
+    feat_expect_true(cat.legCount === 4, "catTest case 3 error!");
+    cat.name = "Tom";
+    feat_expect_true(cat.name === "Tom", "catTest case 4 error!");
+    // read only
+    cat.legCount = 0;
+    feat_expect_true(cat.legCount === 4, "catTest case 5 error!");
+    feat_expect_true(cat.eatFood(["food", "food"]), "catTest case 6 error!");
+    feat_expect_true(cat.run(100, "home"), "catTest case 7 error!");
+    // para error
+    feat_expect_true(cat.eatFood() instanceof Error, "catTest case 8 error!");
+    feat_expect_true(cat.run() instanceof Error, "catTest case 9 error!");
+    feat_expect_true(cat.eatFood(100) instanceof Error, "catTest case 9 error!");
+})
+
+feat_test("interfaceTest", "pigeonTest", () => {
+    let pigeon = test.createPigeon();
+    feat_expect_true(pigeon !== undefined && pigeon !== null, "pigeonTest case 1 error!");
+    feat_expect_true(pigeon.breed ===  0, "pigeonTest case 2 error!");
+    pigeon.breed = 2;
+    feat_expect_true(pigeon.breed === 2, "pigeonTest case 3 error!");
+    let data = pigeon.fly();
+    feat_expect_true(arraysEqual(data, ['fly', 'away']), "pigeonTest case 4 error!");
+})
+
+feat_test("interfaceTest", "cockTest", () => {
+    let cock = test.createCock();
+    feat_expect_true(cock !== undefined && cock !== null, "cockTest case 1 error!");
+    feat_expect_true(cock.breed ===  0, "cockTest case 2 error!");
+    cock.breed = 1;
+    feat_expect_true(cock.breed === 1, "cockTest case 3 error!");
+    let data = cock.fly();
+    feat_expect_true(arraysEqual(data, ['fly', 'away']), "cockTest case 4 error!");
+    feat_expect_true(cock.weight === 0, "cockTest case 5 error!");
+    cock.weight = 1;
+    feat_expect_true(cock.weight === 1, "cockTest case 6 error!");
+    cock.walk().then(a => {
+        feat_expect_true(arraysEqual(a, ['walk', 'away']), "cockTest case 7 error!");
+    }, b => {
+        console.log("cock walk reject: ", b.code);
+    });
+    feat_expect_true(cock.legCount === 4, "cockTest case 8 error!");
+    feat_expect_true(cock.name === "cock", "cockTest case 9 error!");
+    cock.name = "Tom";
+    feat_expect_true(cock.name === "Tom", "cockTest case 10 error!");
+})
---
+++ tests/unit/unit_test_jidl_interface.cpp
@@ -0,0 +1,301 @@
+#include "unit_interface_test.h"
+
+#include "unit_jidl_util.h"
+
+typedef struct interface_test_animal_data {
+    int leg_count;
+    FtString name;
+    int breed;
+    int weight;
+} interface_test_animal_data;
+
+void* create_animal_data(FtString name)
+{
+    interface_test_animal_data* data = (interface_test_animal_data*)malloc(sizeof(interface_test_animal_data));
+    data->leg_count = 4;
+    data->name = (char*)FeatureMalloc(64, FT_STRING);
+    strcpy((char*)data->name, name);
+    data->breed = 0;
+    data->weight = 0;
+    return data;
+}
+
+void destory_animal_data(FeatureInterfaceHandle handle)
+{
+    interface_test_animal_data* data = (interface_test_animal_data*)FeatureGetObjectData(handle);
+    if (data) {
+        if (data->name) {
+            FeatureFreeValue((char*)data->name);
+        }
+        free(data);
+    }
+    FeatureSetObjectData(handle, NULL);
+}
+
+static void unit_interface_cat_finalize(FeatureInterfaceHandle handle)
+{
+    destory_animal_data(handle);
+}
+
+static FtString unit_interface_cat_get_name(FeatureInterfaceHandle handle, AppendData append_data)
+{
+    interface_test_animal_data* data = (interface_test_animal_data*)FeatureGetObjectData(handle);
+    FeatureDupValue((void*)data->name);
+    return data->name;
+}
+
+static void unit_interface_cat_set_name(FeatureInterfaceHandle handle, AppendData append_data, FtString name)
+{
+    interface_test_animal_data* data = (interface_test_animal_data*)FeatureGetObjectData(handle);
+    memset((char*)data->name, 0, 64);
+    strcpy((char*)data->name, name);
+}
+
+static FtInt unit_interface_cat_get_legCount(FeatureInterfaceHandle handle, AppendData append_data)
+{
+    interface_test_animal_data* data = (interface_test_animal_data*)FeatureGetObjectData(handle);
+    return data->leg_count;
+}
+
+static FtBool unit_interface_cat_eatFood(FeatureInterfaceHandle handle, AppendData append_data, FtArray* foods)
+{
+    if (!foods) {
+        return false;
+    }
+    FTArrayHelper<const char*> arrayHelper(foods);
+    for (int i = 0; i < arrayHelper.size(); i++) {
+        if (strcmp(arrayHelper[i], "food"))
+            return false;
+    }
+    return true;
+}
+
+static FtBool unit_interface_cat_run(FeatureInterfaceHandle handle, AppendData append_data, FtInt distance, FtString destination)
+{
+    return distance == 100 && strcmp(destination, "home") == 0;
+}
+
+void unit_interface_test_onRegister(const char* feature_name)
+{
+    return;
+}
+void unit_interface_test_onCreate(FeatureRuntimeContext ctx, FeatureProtoHandle handle)
+{
+    return;
+}
+void unit_interface_test_onRequired(FeatureRuntimeContext ctx, FeatureInstanceHandle handle)
+{
+    auto* name = (char*)malloc(64);
+    memset(name, 0, 64);
+    FeatureSetObjectData(handle, (void*)name);
+    return;
+}
+void unit_interface_test_onDetached(FeatureRuntimeContext ctx, FeatureInstanceHandle handle)
+{
+    auto* name = (char*)FeatureGetObjectData(handle);
+    free(name);
+    FeatureSetObjectData(handle, nullptr);
+    return;
+}
+void unit_interface_test_onDestroy(FeatureRuntimeContext ctx, FeatureProtoHandle handle)
+{
+    return;
+}
+void unit_interface_test_onUnregister(const char* feature_name)
+{
+    return;
+}
+
+FeatureInterfaceHandle unit_interface_test_wrap_createDog(FeatureInstanceHandle feature, AppendData append_data, FtInt type)
+{
+    // test for null ret
+    if (type == 0) {
+        return nullptr;
+    }
+    FeatureInterfaceHandle handle = unit_interface_test_createDog_instance(feature);
+    void* data = create_animal_data("dog");
+    FeatureSetObjectData(handle, data);
+    return handle;
+}
+
+FeatureInterfaceHandle unit_interface_test_wrap_createPigeon(FeatureInstanceHandle feature, AppendData append_data)
+{
+    FeatureInterfaceHandle handle = unit_interface_test_createPigeon_instance(feature);
+    void* data = create_animal_data("pigeon");
+    FeatureSetObjectData(handle, data);
+    return handle;
+}
+
+FeatureInterfaceHandle unit_interface_test_wrap_createCock(FeatureInstanceHandle feature, AppendData append_data)
+{
+    FeatureInterfaceHandle handle = unit_interface_test_createCock_instance(feature);
+    void* data = create_animal_data("cock");
+    FeatureSetObjectData(handle, data);
+    return handle;
+}
+FeatureInterfaceHandle unit_interface_test_wrap_createCat(FeatureInstanceHandle feature, AppendData append_data)
+{
+    static unit_interface_test_Animal_interface_vtable cat_vtable = {
+        .base = { .finalizer = (NativeFunc)unit_interface_cat_finalize },
+        .get_name = unit_interface_cat_get_name,
+        .set_name = unit_interface_cat_set_name,
+        .get_legCount = unit_interface_cat_get_legCount,
+        .eatFood = unit_interface_cat_eatFood,
+        .run = unit_interface_cat_run,
+    };
+    void* data = create_animal_data("cat");
+    FeatureInterfaceHandle handle = unit_interface_test_Animal_create(feature, &cat_vtable, data);
+    return handle;
+}
+
+void unit_interface_test_wrap_setAnimal(FeatureInstanceHandle feature, AppendData append_data, FeatureInterfaceHandle animal)
+{
+    interface_test_animal_data* data = (interface_test_animal_data*)FeatureGetObjectData(animal);
+    char* instance_name = (char*)FeatureGetObjectData(feature);
+    memset(instance_name, 0, 64);
+    strcpy((char*)instance_name, data->name);
+}
+
+FtBool unit_interface_test_wrap_testAnimal(FeatureInstanceHandle feature, AppendData append_data, FtString name)
+{
+    char* instance_name = (char*)FeatureGetObjectData(feature);
+    return strcmp(instance_name, name) == 0;
+}
+
+void unit_interface_test_Animal_interface_dog_finalize(FeatureInterfaceHandle handle)
+{
+    destory_animal_data(handle);
+}
+FtString unit_interface_test_Animal_interface_dog_get_name(FeatureInterfaceHandle handle, AppendData append_data)
+{
+    interface_test_animal_data* data = (interface_test_animal_data*)FeatureGetObjectData(handle);
+    FeatureDupValue((void*)data->name);
+    return data->name;
+}
+void unit_interface_test_Animal_interface_dog_set_name(FeatureInterfaceHandle handle, AppendData append_data, FtString name)
+{
+    interface_test_animal_data* data = (interface_test_animal_data*)FeatureGetObjectData(handle);
+    memset((char*)data->name, 0, 64);
+    strcpy((char*)data->name, name);
+}
+FtInt unit_interface_test_Animal_interface_dog_get_legCount(FeatureInterfaceHandle handle, AppendData append_data)
+{
+    interface_test_animal_data* data = (interface_test_animal_data*)FeatureGetObjectData(handle);
+    return data->leg_count;
+}
+
+FtBool unit_interface_test_Animal_interface_dog_eatFood(FeatureInterfaceHandle handle, AppendData append_data, FtArray* foods)
+{
+    if (!foods) {
+        return false;
+    }
+    FTArrayHelper<const char*> arrayHelper(foods);
+    for (int i = 0; i < arrayHelper.size(); i++) {
+        if (strcmp(arrayHelper[i], "food"))
+            return false;
+    }
+    return true;
+}
+FtBool unit_interface_test_Animal_interface_dog_run(FeatureInterfaceHandle handle, AppendData append_data, FtInt distance, FtString destination)
+{
+    return distance == 100 && strcmp(destination, "home") == 0;
+}
+
+// vtable functions for interface constructor function 'createPigeon'
+void unit_interface_test_Bird_interface_pigeon_finalize(FeatureInterfaceHandle handle)
+{
+    destory_animal_data(handle);
+}
+FtArray* unit_interface_test_Bird_interface_pigeon_fly(FeatureInterfaceHandle handle, AppendData append_data)
+{
+    FtArray* ret = FeatureCreateArray(handle, 2, FT_STRING);
+    FeatureArrayAppendRaw(ret, "fly");
+    FeatureArrayAppendRaw(ret, "away");
+    return ret;
+}
+FtInt unit_interface_test_Bird_interface_pigeon_get_breed(FeatureInterfaceHandle handle, AppendData append_data)
+{
+    interface_test_animal_data* data = (interface_test_animal_data*)FeatureGetObjectData(handle);
+    return data->breed;
+}
+void unit_interface_test_Bird_interface_pigeon_set_breed(FeatureInterfaceHandle handle, AppendData append_data, FtInt breed)
+{
+    interface_test_animal_data* data = (interface_test_animal_data*)FeatureGetObjectData(handle);
+    data->breed = breed;
+}
+
+// vtable functions for interface constructor function 'createCock'
+void unit_interface_test_Chicken_interface_cock_finalize(FeatureInterfaceHandle handle)
+{
+    destory_animal_data(handle);
+}
+FtString unit_interface_test_Chicken_interface_cock_get_name(FeatureInterfaceHandle handle, AppendData append_data)
+{
+    interface_test_animal_data* data = (interface_test_animal_data*)FeatureGetObjectData(handle);
+    FeatureDupValue((void*)data->name);
+    return data->name;
+}
+void unit_interface_test_Chicken_interface_cock_set_name(FeatureInterfaceHandle handle, AppendData append_data, FtString name)
+{
+    interface_test_animal_data* data = (interface_test_animal_data*)FeatureGetObjectData(handle);
+    memset((char*)data->name, 0, 64);
+    strcpy((char*)data->name, name);
+}
+FtInt unit_interface_test_Chicken_interface_cock_get_legCount(FeatureInterfaceHandle handle, AppendData append_data)
+{
+    interface_test_animal_data* data = (interface_test_animal_data*)FeatureGetObjectData(handle);
+    return data->leg_count;
+}
+
+FtBool unit_interface_test_Chicken_interface_cock_eatFood(FeatureInterfaceHandle handle, AppendData append_data, FtArray* foods)
+{
+    if (!foods) {
+        return false;
+    }
+    FTArrayHelper<const char*> arrayHelper(foods);
+    for (int i = 0; i < arrayHelper.size(); i++) {
+        if (strcmp(arrayHelper[i], "food"))
+            return false;
+    }
+    return true;
+}
+FtBool unit_interface_test_Chicken_interface_cock_run(FeatureInterfaceHandle handle, AppendData append_data, FtInt distance, FtString destination)
+{
+    return distance == 100 && strcmp(destination, "home") == 0;
+}
+FtArray* unit_interface_test_Chicken_interface_cock_fly(FeatureInterfaceHandle handle, AppendData append_data)
+{
+    FtArray* ret = FeatureCreateArray(handle, 2, FT_STRING);
+    FeatureArrayAppendRaw(ret, "fly");
+    FeatureArrayAppendRaw(ret, "away");
+    return ret;
+}
+FtInt unit_interface_test_Chicken_interface_cock_get_breed(FeatureInterfaceHandle handle, AppendData append_data)
+{
+    interface_test_animal_data* data = (interface_test_animal_data*)FeatureGetObjectData(handle);
+    return data->breed;
+}
+void unit_interface_test_Chicken_interface_cock_set_breed(FeatureInterfaceHandle handle, AppendData append_data, FtInt breed)
+{
+    interface_test_animal_data* data = (interface_test_animal_data*)FeatureGetObjectData(handle);
+    data->breed = breed;
+}
+FtInt unit_interface_test_Chicken_interface_cock_get_weight(FeatureInterfaceHandle handle, AppendData append_data)
+{
+    interface_test_animal_data* data = (interface_test_animal_data*)FeatureGetObjectData(handle);
+    return data->weight;
+}
+void unit_interface_test_Chicken_interface_cock_set_weight(FeatureInterfaceHandle handle, AppendData append_data, FtInt weight)
+{
+    interface_test_animal_data* data = (interface_test_animal_data*)FeatureGetObjectData(handle);
+    data->weight = weight;
+}
+void unit_interface_test_Chicken_interface_cock_walk(FeatureInterfaceHandle handle, AppendData append_data, FtPromiseId pid)
+{
+    FtArray* ret = FeatureCreateArray(handle, 2, FT_STRING);
+    FeatureArrayAppendRaw(ret, "walk");
+    FeatureArrayAppendRaw(ret, "away");
+    FeaturePromiseResolve(handle, pid, ret);
+    FeatureFreeValue(ret);
+    return;
+}
