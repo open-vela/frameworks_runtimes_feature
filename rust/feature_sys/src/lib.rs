ffbce2ad0a4e44df9d0224d78cb4e3d68fa47f57
wujinfeng <wujinfeng@xiaomi.com>
2025-07-23 13:40:02 +0800

    bring up rust build and add feature_sys directory
    Signed-off-by: wujinfeng <wujinfeng@xiaomi.com>
    Signed-off-by: liujiacai <liujiacai@xiaomi.com>

---
+++ rust/feature_sys/src/lib.rs
@@ -0,0 +1,8 @@
+#![allow(
+    non_snake_case,
+    non_camel_case_types,
+    non_upper_case_globals,
+    unsafe_op_in_unsafe_fn,
+    clippy::all
+)]
+include!(concat!(env!("OUT_DIR"), "/feature_framework.rs"));
